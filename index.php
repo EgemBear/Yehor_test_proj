b00a7fc0e68ea4d2874594d249043ed4fe1fb5f6
yehemberdinov <egemberdinov01@gmail.com>
2018-10-09 22:13:54 +0300

    Мой первый гит

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+This is front controller
+ */
+/**1общие настройки**/
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+/** 2.Подключение файлов системы **/
+/** 3. Подключение БД **/
+/** 4. Вызов Router */
---
+++ routes/routes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yehemberdinov
+ * Date: 05.10.18
+ * Time: 17:27
+ */
+return array(
+    'new' =>'news/index',
+    'products' => 'product/list',
+);
